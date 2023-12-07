a1f8449a2e7324b8678a7f8d0e055163e613d89c
Shthf <89492771+Shthf@users.noreply.github.com>
2023-12-06 20:45:56 -0500

    Update SplitResult.java

--- SplitResult.java
+++ SplitResult.java
@@ -1,24 +1,30 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class SplitResult {
+
     private int featureIndex;
     private double splitValue;
     private List<double[]> leftX;
     private List<Double> leftY;
     private List<double[]> rightX;
     private List<Double> rightY;
+    private double varianceReduction;
 
-    public SplitResult(int featureIndex, double splitValue, List<double[]> leftX, List<Double> leftY,
-                       List<double[]> rightX, List<Double> rightY) {
+    public SplitResult(int featureIndex, double splitValue,
+                       List<double[]> leftX, List<Double> leftY,
+                       List<double[]> rightX, List<Double> rightY,
+                       double varianceReduction) {
         this.featureIndex = featureIndex;
         this.splitValue = splitValue;
-        this.leftX = leftX;
-        this.leftY = leftY;
-        this.rightX = rightX;
-        this.rightY = rightY;
+        this.leftX = new ArrayList<>(leftX);
+        this.leftY = new ArrayList<>(leftY);
+        this.rightX = new ArrayList<>(rightX);
+        this.rightY = new ArrayList<>(rightY);
+        this.varianceReduction = varianceReduction;
     }
 
-    // Getters...
+    // Getters
 
     public int getFeatureIndex() {
         return featureIndex;
@@ -42,5 +48,9 @@ public class SplitResult {
 
     public List<Double> getRightY() {
         return rightY;
+    }
+
+    public double getVarianceReduction() {
+        return varianceReduction;
     }
 }
