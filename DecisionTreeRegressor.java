132669300fe4e3dc759db2696a3735e1e8d37638
Shthf <89492771+Shthf@users.noreply.github.com>
2023-12-06 20:44:28 -0500

    Update DecisionTreeRegressor.java

--- DecisionTreeRegressor.java
+++ DecisionTreeRegressor.java
@@ -1,96 +1,66 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.stream.Collectors;
 
 class DecisionTreeRegressor {
-    //will change to private later
-    public TreeNode root;
+
+    private TreeNode root;
+
+    public DecisionTreeRegressor() {
+        this.root = null;
+    }
 
     public void fit(List<double[]> X, List<Double> y) {
-        root = buildTree(X, y);
+        this.root = buildTree(X, y, 0);  // Start building the tree recursively
     }
 
     public double predict(double[] sample) {
-        return predictRecursive(sample, root);
+        if (root != null) {
+            return predictRecursive(sample, root);
+        }
+        throw new IllegalStateException("Decision tree has not been trained. Call fit() first.");
     }
 
-    private TreeNode buildTree(List<double[]> X, List<Double> y) {
-        // Create a new tree node
-        TreeNode node = new TreeNode();
-
-        // Stopping criteria - check if the node should be a leaf
-        if (stoppingCriteriaMet(X, y)) {
-            // If the stopping criterion is met, make the node a leaf   
-            node.setPrediction(y.stream().mapToDouble(Double::doubleValue).average().orElse(0.0));
-            return node;
+    private TreeNode buildTree(List<double[]> X, List<Double> y, int depth) {
+        if (stoppingCriteriaMet(X, y, depth)) {
+            // Create a leaf node with the mean of target values as the prediction
+            TreeNode leafNode = new TreeNode();
+            leafNode.setLeftChild(null);
+            leafNode.setRightChild(null);
+            leafNode.setPrediction(calculateMean(y));
+            return leafNode;
         }
 
-        // If the stopping criterion is not met, find the best split point
+        // Find the best split
         SplitResult bestSplit = findBestSplit(X, y);
-        node.setSplitFeatureIndex(bestSplit.getFeatureIndex());
-        node.setSplitValue(bestSplit.getSplitValue());
 
-        // Recursively build left and right subtrees
+        if (bestSplit.getVarianceReduction() <= 0.0) {
+            // If no improvement in variance reduction, create a leaf node
+            TreeNode leafNode = new TreeNode();
+            leafNode.setLeftChild(null);
+            leafNode.setRightChild(null);
+            leafNode.setPrediction(calculateMean(y));
+            return leafNode;
+        }
+
+        // Split the data
         List<double[]> leftX = bestSplit.getLeftX();
         List<Double> leftY = bestSplit.getLeftY();
-        node.setLeftChild(buildTree(leftX, leftY));
-
         List<double[]> rightX = bestSplit.getRightX();
         List<Double> rightY = bestSplit.getRightY();
-        node.setRightChild(buildTree(rightX, rightY));
+
+        // Recursively build left and right subtrees
+        TreeNode node = new TreeNode();
+        node.setSplitFeatureIndex(bestSplit.getFeatureIndex());
+        node.setSplitValue(bestSplit.getSplitValue());
+        node.setLeftChild(buildTree(leftX, leftY, depth + 1));
+        node.setRightChild(buildTree(rightX, rightY, depth + 1));
 
         return node;
     }
-
-
-    public void printTree(TreeNode n){
-        if(n.isLeaf()){
-            System.out.println(root.getPrediction());
-        }else{
-            printTree(n.getLeftChild());
-            printTree(n.getRightChild());
-        }   
-    }
-
-
-    private boolean stoppingCriteriaMet(List<double[]> X, List<Double> y) {
-        // Calculate the variance before the split
-        double varianceBeforeSplit = calculateVariance(y);
-    
-        // Perform a potential split and calculate the variance after the split
-        SplitResult bestSplit = findBestSplit(X, y);
-        List<Double> leftY = bestSplit.getLeftY();
-        List<Double> rightY = bestSplit.getRightY();
-        double varianceAfterSplit = calculateVariance(leftY) + calculateVariance(rightY);
-    
-        // Compare the reduction in variance with a threshold
-        double reductionInVariance = varianceBeforeSplit - varianceAfterSplit;
-        double varianceReductionThreshold = 0.01; // You can adjust this threshold based on your needs
-    
-        return reductionInVariance < varianceReductionThreshold;
-    }
-    
-    private double calculateVariance(List<Double> values) {
-        int size = values.size();
-        
-        if (size == 0) {
-            return 0.0;  // Return 0 variance for an empty list
-        }
-    
-        double mean = values.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
-        
-        // Calculate the sum of squared differences from the mean
-        double sumSquaredDifferences = values.stream()
-                .mapToDouble(value -> Math.pow(value - mean, 2))
-                .sum();
-    
-        // Calculate the variance
-        double variance = sumSquaredDifferences / size;
-    
-        return variance;
-    }
-
-
-
 
     private double predictRecursive(double[] sample, TreeNode node) {
         if (node.isLeaf()) {
@@ -107,17 +77,19 @@ class DecisionTreeRegressor {
         }
     }
 
-    
+    private boolean stoppingCriteriaMet(List<double[]> X, List<Double> y, int depth) {
+        // Implement your stopping criteria here, e.g., based on depth or minimum samples in a node.
+        return depth >= 5 || X.size() <= 1;
+    }
+
+    private double calculateMean(List<Double> values) {
+        return values.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
+    }
 
     private SplitResult findBestSplit(List<double[]> X, List<Double> y) {
-        if (X.isEmpty() || y.isEmpty()) {
-            return new SplitResult(-1, 0.0, new ArrayList<>(), new ArrayList<>(), new ArrayList<>(), new ArrayList<>());
-        }
-        
-        
         int numFeatures = X.get(0).length;
         int dataSize = X.size();
-    
+
         int bestFeatureIndex = -1;
         double bestSplitValue = 0.0;
         double bestVarianceReduction = Double.NEGATIVE_INFINITY;
@@ -125,42 +97,35 @@ class DecisionTreeRegressor {
         List<Double> bestLeftY = new ArrayList<>();
         List<double[]> bestRightX = new ArrayList<>();
         List<Double> bestRightY = new ArrayList<>();
-    
+
         for (int featureIndex = 0; featureIndex < numFeatures; featureIndex++) {
-            // Sort data points based on the current feature
             List<DataPoint> sortedData = getSortedData(X, y, featureIndex);
-    
-            // Iterate over potential split points
+
             for (int i = 0; i < dataSize - 1; i++) {
                 double splitValue = (sortedData.get(i).getValue() + sortedData.get(i + 1).getValue()) / 2.0;
-    
-                // Split the data into left and right subsets
+
                 List<double[]> leftX = sortedData.subList(0, i + 1)
                         .stream()
                         .map(DataPoint::getX)
                         .collect(Collectors.toList());
-    
+
                 List<Double> leftY = sortedData.subList(0, i + 1)
                         .stream()
                         .map(DataPoint::getY)
                         .collect(Collectors.toList());
-    
+
                 List<double[]> rightX = sortedData.subList(i + 1, dataSize)
                         .stream()
                         .map(DataPoint::getX)
                         .collect(Collectors.toList());
-    
+
                 List<Double> rightY = sortedData.subList(i + 1, dataSize)
                         .stream()
                         .map(DataPoint::getY)
                         .collect(Collectors.toList());
-    
-                // Calculate variance reduction
-                double varianceBeforeSplit = calculateVariance(y);
-                double varianceAfterSplit = (calculateVariance(leftY) + calculateVariance(rightY)) / 2.0;
-                double varianceReduction = varianceBeforeSplit - varianceAfterSplit;
-    
-                // Update the best split if the current split is better
+
+                double varianceReduction = calculateVarianceReduction(y, leftY, rightY);
+
                 if (varianceReduction > bestVarianceReduction) {
                     bestVarianceReduction = varianceReduction;
                     bestFeatureIndex = featureIndex;
@@ -172,50 +137,88 @@ class DecisionTreeRegressor {
                 }
             }
         }
-    
-        return new SplitResult(bestFeatureIndex, bestSplitValue, bestLeftX, bestLeftY, bestRightX, bestRightY);
+
+        return new SplitResult(bestFeatureIndex, bestSplitValue, bestLeftX, bestLeftY, bestRightX, bestRightY, bestVarianceReduction);
     }
 
-    public String printInfo(List<double[]> dl){
-        String s = "";
+    private double calculateVarianceReduction(List<Double> parentY, List<Double> leftY, List<Double> rightY) {
+        double parentVariance = calculateVariance(parentY);
+        double leftVariance = calculateVariance(leftY);
+        double rightVariance = calculateVariance(rightY);
 
-        for(int i = 0; i < dl.size(); i++){
-            for(int j = 0; j < dl.get(i).length; j++){
-                s += dl.get(i)[j] + " ";
-            }
-            s += "\n";
-        }
-        
-        return s;
+        int parentSize = parentY.size();
+        int leftSize = leftY.size();
+        int rightSize = rightY.size();
+
+        double weightedAverageVariance = (leftSize * leftVariance + rightSize * rightVariance) / parentSize;
+
+        return parentVariance - weightedAverageVariance;
     }
 
-    public String printInfo2(List<Double> dl){
-        String s = "";
-
-        for(int i = 0; i < dl.size(); i++){
-            
-                s += dl.get(i).toString() + " ";
-            
-            s += "\n";
-        }
-        
-        return s;
-    }
 
     private List<DataPoint> getSortedData(List<double[]> X, List<Double> y, int featureIndex) {
         List<DataPoint> data = new ArrayList<>();
-    
+
         // Combine features and labels into DataPoint objects
         for (int i = 0; i < X.size(); i++) {
             double[] features = X.get(i);
             double label = y.get(i);
             data.add(new DataPoint(features, label, featureIndex));
         }
-    
+
         // Sort DataPoint objects based on the specified feature index
         data.sort(Comparator.comparingDouble(dp -> dp.getX()[featureIndex]));
-    
+
         return data;
+    }
+
+    private double calculateVariance(List<Double> values) {
+        int size = values.size();
+
+        if (size == 0) {
+            return 0.0;  // Return 0 variance for an empty list
+        }
+
+        double mean = values.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
+
+        // Calculate the sum of squared differences from the mean
+        double sumSquaredDifferences = values.stream()
+                .mapToDouble(value -> Math.pow(value - mean, 2))
+                .sum();
+
+        // Calculate the variance
+        double variance = sumSquaredDifferences / size;
+
+        return variance;
+    }
+
+    //Print the tree in breadthfirst order for inspection
+    public void printTreeBreadthFirst() {
+        if (root == null) {
+            return;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode node = queue.poll();
+
+                if (node.isLeaf()) {
+                    System.out.print("(Leaf: " + node.getPrediction() + ")  ");
+                } else {
+                    System.out.print("(" + node.getSplitFeatureIndex() +
+                            " -> " + node.getSplitValue() + " ) ");
+
+                    queue.add(node.getLeftChild());
+                    queue.add(node.getRightChild());
+                }
+            }
+            System.out.println(); // Move to the next level in the output
+        }
     }
 
     private static class DataPoint implements Comparable<DataPoint> {
